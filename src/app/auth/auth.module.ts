362250ccde5e0a8df6262c155e5f100db83544ae
RuddyQuispe <ruddyq18@gmail.com>
2024-09-01 19:27:47 -0400

    build layout and auth page advance

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from "./auth-routing.modeulo";
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
-
+import {LayoutPageComponent} from './pages/layout-page/layout-page.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
+import {MaterialModule} from "../material/material.module";
 
 
 @NgModule({
@@ -15,7 +15,9 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
